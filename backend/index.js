fab236228c1cb7a4a247e583b9e82871505f4535
Valdinei Jacques <valdineij@gmail.com>
2022-01-19 13:18:53 -0400

    Allow pets with no images

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.use(express.json())
 
 app.use(cors({ credentials: true, origin: 'http://localhost:3000' }))
 
-app.use(express.static('public'))
+app.use(express.static('public', { extensions: ['png'] }))
 const UserRoutes = require('./routes/UserRoutes')
 const PetRoutes = require('./routes/PetRoutes')
 app.use('/users', UserRoutes)
